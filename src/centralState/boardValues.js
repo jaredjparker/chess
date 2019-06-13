c25c6b1e6ad7999596c0ae52ce4ceecea0c87aa7
VIISilver <jparke71@hotmail.com>
2019-06-12 18:42:40 -0600

    Committing right before potentially breaking changes are added.

---
+++ src/centralState/boardValues.js
@@ -0,0 +1,13 @@
+// INITIAL STATE
+const boardValues = {
+    a8: 'Dark_Rook',
+    b8: 'Dark_Knight',
+    c8: 'Dark_Bishop',
+    d8: 'Dark_King',
+    e8: 'Dark_Queen',
+    f8: 'Dark_Bishop',
+    g8: 'Dark_Knight',
+    h8: 'Dark_Rook',
+}
+
+export default boardValues;
--- src/components/chess/board/Squares.js
+++ src/components/chess/board/Squares.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import { validSquareSelect } from '../../../actions/gameActions';
-import initialState from '../../../centralState/gameState';
 import './Squares.css';
 import pawnDk from '../../../assets/Chess_pdt60.png';
 import pawnLt from '../../../assets/Chess_plt60.png';
 import bishopDk from '../../../assets/Chess_bdt60.png';
 import bishopLt from '../../../assets/Chess_blt60.png';
-import kingDk from '../../../assets/Chess_kdt60.png';
+import King_Dark from '../../../assets/Chess_kdt60.png';
 import kingLt from '../../../assets/Chess_klt60.png';
 import knightDk from '../../../assets/Chess_ndt60.png';
 import knightLt from '../../../assets/Chess_nlt60.png';
@@ -19,12 +18,16 @@ import rookLt from '../../../assets/Chess_rlt60.png';
 export default class Squares extends Component {
     constructor(props) {
       super(props);
+      
+      const stateSquareId = this.props.oneLtrColumnId.concat(this.props.snglDigRowId);
+
       this.state = {
           selectedSquare: false,
           squareInfo: '',
+          squareImage: '',
           rowId: this.props.snglDigRowId,
           columnId: this.props.oneLtrColumnId,
-          squareId: this.props.oneLtrColumnId.concat(this.props.snglDigRowId)
+          squareId: stateSquareId
         };
     }
 
@@ -32,31 +35,31 @@ export default class Squares extends Component {
       const squareId = this.props.oneLtrColumnId.concat(this.props.snglDigRowId);
         switch(squareId) {
           case 'd8':
-            return this.setState({ squareInfo: `King Dark ${squareId}`});
+            return this.setState({ squareInfo: `King_Dark ${squareId}`, squareImage: King_Dark});
           case 'd1':
-            return this.setState({ squareInfo: `King Light ${squareId}`});
+            return this.setState({ squareInfo: `King Light ${squareId}`, squareImage: kingLt});
           case 'e8':
-            return this.setState({ squareInfo: `Queen Dark ${squareId}`});
+            return this.setState({ squareInfo: `Queen Dark ${squareId}`, squareImage: queenDk});
           case 'e1':
-            return this.setState({ squareInfo: `Queen Light ${squareId}`});
+            return this.setState({ squareInfo: `Queen Light ${squareId}`, squareImage: queenLt});
           case 'c8':
           case 'f8':
-            return this.setState({ squareInfo: `Bishop Dark ${squareId}`});
+            return this.setState({ squareInfo: `Bishop Dark ${squareId}`, squareImage: bishopDk});
           case 'c1':
           case 'f1':
-            return this.setState({ squareInfo: `Bishop Light ${squareId}`});
+            return this.setState({ squareInfo: `Bishop Light ${squareId}`, squareImage: bishopLt});
           case 'b8':
           case 'g8':
-            return this.setState({ squareInfo: `Knight Dark ${squareId}`});
+            return this.setState({ squareInfo: `Knight Dark ${squareId}`, squareImage: knightDk});
           case 'b1':
           case 'g1':
-            return this.setState({ squareInfo: `Knight Light ${squareId}`});
+            return this.setState({ squareInfo: `Knight Light ${squareId}`, squareImage: knightLt});
           case 'a8':
           case 'h8':
-            return this.setState({ squareInfo: `Rook Dark ${squareId}`});
+            return this.setState({ squareInfo: `Rook Dark ${squareId}`, squareImage: rookDk});
           case 'a1':
           case 'h1':
-            return this.setState({ squareInfo: `Rook Light ${squareId}`});
+            return this.setState({ squareInfo: `Rook Light ${squareId}`, squareImage: rookLt});
           case 'a7':
           case 'b7':
           case 'c7':
@@ -65,7 +68,7 @@ export default class Squares extends Component {
           case 'f7':
           case 'g7':
           case 'h7':
-            return this.setState({ squareInfo: `Pawn Dark ${squareId}`});
+            return this.setState({ squareInfo: `Pawn Dark ${squareId}`, squareImage: pawnDk});
           case 'a2':
           case 'b2':
           case 'c2':
@@ -74,9 +77,9 @@ export default class Squares extends Component {
           case 'f2':
           case 'g2':
           case 'h2':
-            return this.setState({ squareInfo: `Pawn Light ${squareId}`});
+            return this.setState({ squareInfo: `Pawn Light ${squareId}`, squareImage: pawnLt});
           default:
-              return this.setState({ squareInfo: `Empty ${squareId}`});
+              return this.setState({ squareInfo: `Empty ${squareId}`, squareImage: ''});
       }
 
     }
@@ -87,33 +90,35 @@ export default class Squares extends Component {
     }
 
     piecesPlacement( squareId ) {
+      const { squareImage } = this.state;
+
         switch(squareId) {
             case 'd8':
-              return <img alt={`King Dark ${squareId}`} value={squareId} src={kingDk} />;
+              return <img alt={`King Dark ${squareId}`} value={squareId} src={squareImage} />;
             case 'd1':
-              return <img alt={`King Light ${squareId}`} value={squareId} src={kingLt} />;
+              return <img alt={`King Light ${squareId}`} value={squareId} src={squareImage} />;
             case 'e8':
-              return <img alt={`Queen Dark ${squareId}`} value={squareId} src={queenDk} />;
+              return <img alt={`Queen Dark ${squareId}`} value={squareId} src={squareImage} />;
             case 'e1':
-              return <img alt={`Queen Light ${squareId}`} value={squareId} src={queenLt} />;
+              return <img alt={`Queen Light ${squareId}`} value={squareId} src={squareImage} />;
             case 'c8':
             case 'f8':
-              return <img alt={`Bishop Dark ${squareId}`} value={squareId} src={bishopDk} />;
+              return <img alt={`Bishop Dark ${squareId}`} value={squareId} src={squareImage} />;
             case 'c1':
             case 'f1':
-              return <img alt={`Bishop Light ${squareId}`} value={squareId} src={bishopLt} />;
+              return <img alt={`Bishop Light ${squareId}`} value={squareId} src={squareImage} />;
             case 'b8':
             case 'g8':
-              return <img alt={`Knight Dark ${squareId}`} value={squareId} src={knightDk} />;
+              return <img alt={`Knight Dark ${squareId}`} value={squareId} src={squareImage} />;
             case 'b1':
             case 'g1':
-              return <img alt={`Knight Light ${squareId}`} value={squareId} src={knightLt} />;
+              return <img alt={`Knight Light ${squareId}`} value={squareId} src={squareImage} />;
             case 'a8':
             case 'h8':
-              return <img alt={`Rook Dark ${squareId}`} value={squareId} src={rookDk} />;
+              return <img alt={`Rook Dark ${squareId}`} value={squareId} src={squareImage} />;
             case 'a1':
             case 'h1':
-              return <img alt={`Rook Light ${squareId}`} value={squareId} src={rookLt} />;
+              return <img alt={`Rook Light ${squareId}`} value={squareId} src={squareImage} />;
             case 'a7':
             case 'b7':
             case 'c7':
@@ -122,7 +127,7 @@ export default class Squares extends Component {
             case 'f7':
             case 'g7':
             case 'h7':
-              return <img alt={`Pawn Dark ${squareId}`} value={squareId} src={pawnDk} />;
+              return <img alt={`Pawn Dark ${squareId}`} value={squareId} src={squareImage} />;
             case 'a2':
             case 'b2':
             case 'c2':
@@ -131,7 +136,7 @@ export default class Squares extends Component {
             case 'f2':
             case 'g2':
             case 'h2':
-              return <img alt={`Pawn Light ${squareId}`} value={squareId} src={pawnLt} />;
+              return <img alt={`Pawn Light ${squareId}`} value={squareId} src={squareImage} />;
             default:
                 return <img alt={''} value={squareId} src={''} />
         }
